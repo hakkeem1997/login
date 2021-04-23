2dcc5460ea046d25c6e23336fdc7e03d029198b7
hakkeem1997 <79262803+hakkeem1997@users.noreply.github.com>
2021-04-23 15:30:07 +0530

    Revert "Update"
    This reverts commit cb572e432f766da78fdcf943ab251d41540a7308.

--- login/src/app/app.module.ts
+++ login/src/app/app.module.ts
@@ -1,19 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-
-
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
-import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
+import { AdminGuard } from './admin.guard';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy, }],
-  
-  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    SignupComponent,
+
+    DashboardComponent,
+    HomeComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule, HttpClientModule, FormsModule,ReactiveFormsModule
+  ],
+  providers: [AdminGuard],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
